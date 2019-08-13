c1ac3b71936953aa64345b7e8cef7f27c327cd63
Grex <andrew@grexa.me>
2019-08-13 17:43:57 -0600

    feat: validate CircleCI config before commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   '*.{graphql,json,md,yml}': ['prettier-eslint --list-different --write', 'git add -f'],
   '*.{js,jsx,mjs,ts,tsx}': ['eslint --fix --quiet', 'git add -f'],
+  '*': [() => 'yarn circle:validate'],
   //   "*.{js,ts}": [
   //     "yarn test --bail --collectCoverage=false --passWithNoTests --findRelatedTests"
   //   ],
